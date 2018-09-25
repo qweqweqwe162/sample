085e3a259490ae5da100f1cf0aad82af9502d9b7
qweqweqwe162 <maxim_pristupa@hotmail.com>
2018-09-25 23:41:56 +0300

    Finish Signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new', via: 'get'
+
+	  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
